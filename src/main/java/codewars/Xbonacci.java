024b7e5b5018248df0fb2a6bd68594ca495e4345
Gleb Saharov <saharov.g.m@ulmart.ru>
2019-04-16 16:06:08 +0300

    add Xbonacci

--- src/main/java/codewars/Xbonacci.java
+++ src/main/java/codewars/Xbonacci.java
@@ -1,16 +1,21 @@
 package codewars;
 
 import java.util.Arrays;
+import java.util.stream.DoubleStream;
 
 public class Xbonacci {
 
     public static void main(String... args) {
-        System.out.println(Arrays.toString(new Tribonacci().tribonacci(new double[]{1, 1, 1}, 10)));
+        System.out.println(Arrays.toString(new Xbonacci().xbonacci(new double[]{0, 0, 0, 1}, 20)));
     }
 
     public double[] xbonacci(double[] signature, int n) {
-        // hackonacci me
-        return null;
+        double[] r = Arrays.copyOf(signature, n);
+        for (int i = signature.length; i < n; i++) {
+            double[] subarray = Arrays.copyOfRange(r, i - signature.length, i);
+            r[i] = DoubleStream.of(subarray).sum();
+        }
+        return r;
     }
 
 }
